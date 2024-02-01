3656ef3321e1f40ed61b332c71d47052abad5dae
dns147 <rarden@ya.ru>
2024-02-01 22:52:28 +0300

    feat: add greetings and farewell

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+import * as readline from 'node:readline';
 import { getUserName } from "./src/user.mjs";
 
+const userName = getUserName();
+console.log(`Welcome to the File Manager, ${userName}!\n`);
 
-console.log(`Welcome to the File Manager, ${getUserName()}!`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const eventExit = (line) => {
+  if (line === '.exit') {
+    console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0);
+  }
+};
+
+const eventSigint = () => {
+  console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+  process.exit(0);
+};
+
+rl.on('line', eventExit)
+  .on('SIGINT', eventSigint);
